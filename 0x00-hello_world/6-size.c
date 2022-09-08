54f1295f96e2afd653e133d52ba3697e185ceab7
benjieCie <musili.benjamin@gmail.com>
2022-09-08 10:41:13 -0700

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+	printf("Size of a float: %lu byte(s)", sizeof(e));
 	return (0);
 }
