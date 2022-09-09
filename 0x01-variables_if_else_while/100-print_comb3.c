f6d0d59a682f6bea16d2949611d99955b0da4039
benjieCie <musili.benjamin@gmail.com>
2022-09-09 13:40:45 -0700

    Print Combinations 2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 0; first <= 9; first++)
+	{
+		for (second = first + 1; second <= 9; second++)
+		{
+			putchar(first + '0');
+			putchar(second + '0');
+
+			if (first < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
