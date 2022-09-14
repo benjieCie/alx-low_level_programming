aae210ec3ab4cf3418dc63eda8594f4b481313c7
benjieCie <musili.benjamin@gmail.com>
2022-09-14 10:06:26 -0700

    Correcting hour and min calculation

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,29 +7,19 @@
  */
 void jack_bauer(void)
 {
-	int h1;
-	int h2;
-	int m1;
-	int m2;
+	int hr;
+	int min;
 
-	for (h1 = 48; h1 <= 50; h1++)
+	for (hr = 0; h1 < 24; h1++)
 	{
-		for (h2 = 48; h2 <= 57; h2++)
+		for (min = 0; min < 60; min++)
 		{
-			if (h1 > 50 && h2 > 52)
-				break;
-			for (m1 = 48; m1 <= 53; m1++)
-			{
-				for (m2 = 48; m2 <= 57; m2++)
-				{
-					_putchar(h1);
-					_putchar(h2);
-					_putchar(58);
-					_putchar(m1);
-					_putchar(m2);
-					_putchar('\n');
-				}
-			}
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(58);
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
