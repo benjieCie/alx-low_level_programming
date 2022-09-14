dae095bdc95f8cb32dbfd55d2b839b499a16fafb
benjieCie <musili.benjamin@gmail.com>
2022-09-14 10:27:08 -0700

    a function that prints the 9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: Void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if ((product < 10) && (y !=0))
+			{
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_pytchar('\n');
+	}	
+}
