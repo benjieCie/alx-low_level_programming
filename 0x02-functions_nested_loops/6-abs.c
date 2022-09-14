279f4f423d83ec28de32679aa3bc28c156972e8b
benjieCie <musili.benjamin@gmail.com>
2022-09-14 07:59:15 -0700

    Calculate absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the asbolute value
+ *
+ * @n: number as integer
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
