44fec9b4fb9a7471bbb35fd9f4d6f941ee7381e2
benjieCie <musili.benjamin@gmail.com>
2022-09-14 08:49:15 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+
+	for (h1 = 48; h1 <= 50; h1++)
+	{
+		for (h2 = 48; h2 <= 57; h2++)
+		{
+			for (m1 = 48; m1 <= 54; m1++)
+			{
+				for (m2 = 48; m2 <= 57; m2++)
+				{
+					if (h1 > 50 && h2 >= 52)
+						break;
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(58);
+					_putchar(m1);
+					_putchar(m2);
+					_putchar('\n');
+				}
+			}
+		}	
+	}
+}
