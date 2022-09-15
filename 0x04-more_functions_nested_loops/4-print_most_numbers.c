9e215172d9e5ab4ca7ae0e1fc3c335ad2422c380
benjieCie <musili.benjamin@gmail.com>
2022-09-15 05:47:51 -0700

    Function that prints Mostly numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all numbers except 2 and 4
+ *
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+	}
+	_putchar('\n');
+}
