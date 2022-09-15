833060ad0a9c15ca4765f429e0a9ce9123f3dc4e
benjieCie <musili.benjamin@gmail.com>
2022-09-15 09:32:59 -0700

    Prime Factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry block
+ *
+ * Return: 0 Always success
+ */
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			number = number / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
