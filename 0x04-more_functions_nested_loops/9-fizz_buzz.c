ecb1c94b4cd1b2879964c4c011e3214a2332c4d7
benjieCie <musili.benjamin@gmail.com>
2022-09-15 08:24:22 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints thr numbers from 1 - 100
+ * replaces multpiles of 3 with Fizz
+ * replaces multiples of 5 with Buzz
+ * replaces Multiples of 3 and 5 with FizzBuzz
+ *
+ * Return: 0 Always success
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 101; count++)
+	{
+		if ((count % 3 == 0) && !(count % 5 == 0))
+			printf("Fizz ");
+		else if ((count % 5 == 0) && !(count % 3 == 0))
+			printf("Buzz ");
+		else if ((count % 3 == 0) && (count % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", count);
+	}
+	return (0);
+}
