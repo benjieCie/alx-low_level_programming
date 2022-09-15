febfeafc7c1ed33e1a62687dac973a69cd940c3f
benjieCie <musili.benjamin@gmail.com>
2022-09-15 07:34:54 -0700

    Function to draw a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square based on the number entered
+ *
+ * @size: the size of the square
+ *
+ * Return: Void
+ */
+void print_square(int size)
+{
+	int col;
+	int row;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (col = 1; col <= size; col++)
+		{
+			for (row = 1; row <= size; row++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
