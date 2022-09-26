deb3ac50dce954bf9e37f58457df08a96c871805
benjieCie <musili.benjamin@gmail.com>
2022-09-26 10:43:46 -0700

    Re-thought the loop

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,10 +13,14 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i;
+	int j;
 
-	for (i = 0; i < n; i++)
+	while (i < 0)
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
+		i++;
+		j++;
+		n--;
 	}
 	return (dest);
 }
