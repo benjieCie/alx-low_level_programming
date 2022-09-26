2396f39bd227d3e688b17c3b61a5799a212f2b89
benjieCie <musili.benjamin@gmail.com>
2022-09-26 10:30:19 -0700

    Betty Style: Added return description and memcpy description

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * _memcpy() - function copies n bytes from memory area src 
+ * _memcpy - function copies n bytes from memory area src
  * to memory area dest
  *
  * @dest: destination to copy to
  * @src: Source to copy from
  * @n: bytes to copy
+ *
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
