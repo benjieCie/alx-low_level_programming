449ec2cc813b65a184c98c32a0edfc71daf05fb3
benjieCie <musili.benjamin@gmail.com>
2022-10-14 23:58:08 -0700

    Included stdarg

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(valist, const unsigned int));
 
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
