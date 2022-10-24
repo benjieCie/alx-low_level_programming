209bd29f86bbba7645fdfbe5d3428204cb67a047
benjieCie <musili.benjamin@gmail.com>
2022-10-24 10:58:14 -0700

    Corrected function logic

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,19 @@ size_t print_list(const list_t *h)
 	int i;
 
 	if (h == NULL)
-	{
-		printf("[0] (nil)");
 		return (0);
-	}
-	for (i = 0; h != NULL; i++)
+	while (h != NULL)
 	{
-		printf("[%d] %s", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
+		i++;
 	}
-
 	return (i);
 }
