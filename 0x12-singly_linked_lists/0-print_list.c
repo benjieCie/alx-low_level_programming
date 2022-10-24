7864bbcbca206ea929da7b90e47af424388876ea
benjieCie <musili.benjamin@gmail.com>
2022-10-24 10:29:20 -0700

    function that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ *
+ * @h: Single linked list
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		_putchar(n->str);
+		i++;
+	}
+	return (i);
+}
