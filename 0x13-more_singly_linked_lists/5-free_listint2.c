d8f2bbf17e7e562aa22d172c5824f69b133ad7a0
benjieCie <musili.benjamin@gmail.com>
2022-10-27 06:20:27 -0700

    Changed function logic

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,18 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *temp, *current;
+
 	if (*head == NULL)
 		return;
 
-	while (*head)
+	current = *head;
+
+	while (current != NULL)
 	{
-		free(*head);
-		*head = (*head)->next;
+		temp = current;
+		current = current->next;
+		free(temp);
 	}
 
 	*head = NULL;
